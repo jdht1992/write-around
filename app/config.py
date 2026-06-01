dd1c81d551ca6f18f0e86992622ab6ff8a903fe2
diego herrera <jdht1992@gmail.com>
2026-06-01 17:12:18 -0600

    change setting

--- app/config.py
+++ app/config.py
@@ -7,9 +7,9 @@ class Settings(BaseSettings):
     DATABASE_URL: PostgresDsn 
     REDIS_URL: RedisDsn
 
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
+    #postgres_user: str
+    #postgres_password: str
+    #postgres_db: str
     
     model_config = SettingsConfigDict(env_file=".env", extra='ignore')
 
